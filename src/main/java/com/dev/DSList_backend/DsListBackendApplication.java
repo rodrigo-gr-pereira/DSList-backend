2c1a5b3265e5951e81099f62d626ed8223d680df
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2025-01-22 21:13:24 -0300

    Project Created

---
+++ src/main/java/com/dev/DSList_backend/DsListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.DSList_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListBackendApplication.class, args);
+	}
+
+}
